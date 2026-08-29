84ca3a6f6aedc50593f65eba8c1e3728ebee8717
Navneet Nainwal <107826678+navneet0305@users.noreply.github.com>
2026-08-29 21:13:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main()
 {
-   printf("Hello My name is Anmol Gupta\nI was the cr of section T\n My roll No was 10\n My best frind name is Harshit,Atharva\n");
+   printf("Hello My name is Navneet Nainwal\nI was the cr of section T\n My roll No was 10\n My best frind name is Harshit,Atharva\n");
 }
